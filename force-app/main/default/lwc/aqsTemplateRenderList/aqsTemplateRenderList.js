75228846f1a0df1f09d13344fcc53a87a20bb348
MACCRESI\jhon.cortes <jhon.cortes@maccresi.com>
2025-05-31 18:21:10 +0200

    [main] : Lwc MOA -  Add render list example

---
+++ force-app/main/default/lwc/aqsTemplateRenderList/aqsTemplateRenderList.js
@@ -0,0 +1,18 @@
+/**
+ * @description       : 
+ * @author            : jhon.cortes@maccresi.com
+ * @group             : 
+ * @last modified on  : 05-31-2025
+ * @last modified by  : jhon.cortes@maccresi.com
+**/
+import { LightningElement } from 'lwc';
+
+export default class AqsTemplateRenderList extends LightningElement {
+
+    contactos = [
+    { id: 1, nombre: 'Ana López', email: 'ana@example.com' },
+    { id: 2, nombre: 'Carlos Pérez', email: 'carlos@example.com' },
+    { id: 3, nombre: 'Lucía Gómez', email: 'lucia@example.com' }
+  ];
+
+}
